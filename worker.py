cc72a721c4be0c544e8d64f173455cf5924531ea
Jordan Hayes <hayesjordan@gmail.com>
2021-04-29 11:05:13 -0700

    Pass agrs for CPU and MEM

--- worker.py
+++ worker.py
@@ -2,11 +2,15 @@
 
 import time
 import rediswq
+import os
+import sys
 
 host="redis"
-# Uncomment next two lines if you do not have Kube-DNS working.
-import os
+# Uncomment next line if you do not have Kube-DNS working.
 #host = os.getenv("REDIS_SERVICE_HOST")
+
+CPU=sys.argv[1]
+MEM=sys.argv[2]
 
 q = rediswq.RedisWQ(name="job2", host="redis")
 print("Worker with sessionID: " +  q.sessionID())
@@ -16,7 +20,7 @@ while not q.empty():
   if item is not None:
     itemstr = item.decode("utf-8")
     print("Working on " + itemstr)
-    os.system("conda run --no-capture-output -n myenv bash ./pipeline/assemble/02_plasmidspades.sh %(ARRAY_ID)s %(CPU)s %(MEM)s" % {'ARRAY_ID':itemstr,'CPU':16,'MEM':20})
+    os.system("conda run --no-capture-output -n myenv bash ./pipeline/assemble/02_plasmidspades.sh %(ARRAY_ID)s %(CPU)s %(MEM)s" % {'ARRAY_ID':itemstr,'CPU':CPU,'MEM':MEM})
     q.complete(item)
   else:
     print("Waiting for work")
